bfdfb28b0dc83e1ec60227ed614fe535aac45e90
武浩然 <1252198830@qq.com>
2022-03-08 14:37:39 +0800

    feat: update Subtract

--- src/types/Subtract.ts
+++ src/types/Subtract.ts
@@ -26,3 +26,28 @@ export type Subtract<A extends number, B extends number> =
 type A = Subtract<1, 1> // 0
 type B = Subtract<10, 3> // 7
 type C = Subtract<3, 10> // never
+
+
+/**
+ * @name SubtractHelper
+ * @description 用来帮助计算 Subtract1，前提条件：A肯定大于B
+ */
+export type SubtractHelper1<A extends number, B extends number> = 
+    NumberToTuple<A> extends [...NumberToTuple<B>, ...infer R]
+        ? R['length']
+        : never
+
+/**
+ * @name Subtract1
+ * @description Similar to 38. implement Add<A, B>, please implement Subtract1<A, B>.
+ * @description 1.only need to consider positive integers
+ * @description 2.B is guaranteed to be smaller or equal to A
+ * @example type A = Subtract1<1, 1> // 0
+ * @example type B = Subtract1<10, 3> // 7
+ * @example type C = Subtract1<3, 10> // never
+ */
+export type Subtract1<A extends number, B extends number> = 
+    LargerThan2<A, B> extends false
+        ? never
+        : SubtractHelper1<A, B>
+        
